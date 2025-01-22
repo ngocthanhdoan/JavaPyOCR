3a32b5e2dbb16988dd2e2f665108f50dc1d65e27
Ngọc Thành Đoàn <55902150+ngocthanhdoan@users.noreply.github.com>
2025-01-22 07:29:22 +0000

    remove model

--- src/main/java/com/web/app/ocrweb/pythonmodel/VietOcrModel.java
+++ src/main/java/com/web/app/ocrweb/pythonmodel/VietOcrModel.java
@@ -1,31 +1,31 @@
-package com.web.app.ocrweb.pythonmodel;
+// package com.web.app.ocrweb.pythonmodel;
 
-public enum VietOcrModel {
-    // Định nghĩa các hằng số cho các mô hình OCR
-    VIETOCR_VGGSEQ2SEQ("vgg_seq2seq"),
-    VIETOCR_TRANSFORMER("vgg_transformer"),
+// public enum VietOcrModel {
+//     // Định nghĩa các hằng số cho các mô hình OCR
+//     VIETOCR_VGGSEQ2SEQ("vgg_seq2seq"),
+//     VIETOCR_TRANSFORMER("vgg_transformer"),
 
 
-    // Thuộc tính để lưu trữ tên mô hình
-    private final String modelName;
+//     // Thuộc tính để lưu trữ tên mô hình
+//     private final String modelName =null;
  
-    // Constructor cho enum
-    VietOcrModel(String modelName) {
-        this.modelName = modelName;
-    }
+//     // Constructor cho enum
+//     VietOcrModel(String modelName) {
+//         this.modelName = modelName;
+//     }
 
-    // Phương thức để lấy tên mô hình
-    public String getModelName() {
-        return modelName;
-    }
+//     // Phương thức để lấy tên mô hình
+//     public String getModelName() {
+//         return modelName;
+//     }
 
-    // Phương thức để lấy mô hình từ tên
-    public static VietOcrModel fromModelName(String modelName) {
-        for (VietOcrModel model : VietOcrModel.values()) {
-            if (model.getModelName().equalsIgnoreCase(modelName)) {
-                return model;
-            }
-        }
-        throw new IllegalArgumentException("No model found with name: " + modelName);
-    }
-}
+//     // Phương thức để lấy mô hình từ tên
+//     public static VietOcrModel fromModelName(String modelName) {
+//         for (VietOcrModel model : VietOcrModel.values()) {
+//             if (model.getModelName().equalsIgnoreCase(modelName)) {
+//                 return model;
+//             }
+//         }
+//         throw new IllegalArgumentException("No model found with name: " + modelName);
+//     }
+// }
